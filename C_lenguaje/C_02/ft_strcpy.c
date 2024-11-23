133a8214016c5cf211ab0b4102453070aa8a60c9
Manuel <79838253+ManuelBarra@users.noreply.github.com>
2024-11-23 22:31:43 +0100

    Add strcpy function

---
+++ C_lenguaje/C_02/ft_strcpy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+char *ft_strcpy(char *dest, char *src)
+{
+    int i;
+    i = 0;
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return dest;
+}
+int main()
+{
+    char src[] = "Welcome to the universe";
+    char dest[50];
+
+    ft_strcpy(dest, src);
+    printf("%s\n", dest);	
+     
+    return(0);
+}
